26d5370dc17ea1d63491c914357dceb214a7a57e
mskx4 <mskx4@outlook.com>
2019-12-13 03:23:14 +0100

    hybrid now works

--- RecLib/HybridRecommender.py
+++ RecLib/HybridRecommender.py
@@ -5,6 +5,7 @@ from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 import numpy as np
 
 
+
 class HybridRecommender(BaseRecommender):
     RECOMMENDER_NAME = "HybridRecommender"
 
@@ -24,29 +25,66 @@ class HybridRecommender(BaseRecommender):
                                     If None, all items are computed, otherwise discarded items will have as score -np.inf
     :return:                    array (len(user_id_array), n_items) with the score.
     """
-    def _compute_item_score(self, user_id_array, items_to_compute = None):
-        x = np.arange(0, self.n_users)
-        cold_users_train = x[self._cold_user_mask]
+    # def _compute_item_score(self, user_id_array, items_to_compute=None):
+    #     x = np.arange(0, self.n_users)
+    #     cold_users_train = x[self._cold_user_mask]
+    #
+    #     # Cold & Warm Users ID
+    #     cold_user_id_array = np.intersect1d(cold_users_train, user_id_array)
+    #     warm_user_id_array = np.setdiff1d(user_id_array, cold_user_id_array)
+    #
+    #
+    #     item_weights_1 = self.topPop._compute_item_score(cold_user_id_array, items_to_compute)
+    #     item_weights_2 = self.CF._compute_item_score(warm_user_id_array, items_to_compute)
+    #
+    #
+    #     if items_to_compute is None:
+    #         item_weights = np.empty((len(user_id_array), self.n_items))
+    #     else:
+    #         item_weights = np.empty((len(user_id_array), len(items_to_compute)))
+    #
+    #     coldCounter: int = 0
+    #     warmCounter: int = 0
+    #     for idx, user_id in enumerate(user_id_array):
+    #         if user_id in cold_user_id_array:
+    #             item_weights[idx] = item_weights_1[coldCounter]
+    #             coldCounter = coldCounter + 1
+    #         elif user_id in warm_user_id_array:
+    #             item_weights[idx] = item_weights_2[warmCounter]
+    #             warmCounter = warmCounter + 1
+    #         else:
+    #             raise Exception("ERROR IN ITEM SCORE COMPUTATION")
+    #     return item_weights
+    
+    def recommend(self, user_id_array, cutoff=None, remove_seen_flag=True, items_to_compute=None,
+                  remove_top_pop_flag=False, remove_custom_items_flag=False, return_scores=False):
 
-        # Cold & Warm Users ID
-        cold_user_id_array = np.intersect1d(cold_users_train, user_id_array)
-        warm_user_id_array = np.setdiff1d(user_id_array, cold_user_id_array)
-
-        item_weights_1 = self.topPop._compute_item_score(cold_user_id_array, items_to_compute)
-        item_weights_2 = self.CF._compute_item_score(warm_user_id_array, items_to_compute)
-
-        if items_to_compute is None:
-            item_weights = np.empty((len(user_id_array), self.n_items))
+        # If is a scalar transform it in a 1-cell array
+        if np.isscalar(user_id_array):
+            user_id_array = np.atleast_1d(user_id_array)
+            single_user = True
         else:
-            item_weights = np.empty((len(user_id_array), len(items_to_compute)))
+            single_user = False
 
-        coldCounter: int = 0
-        warmCounter: int = 0
-        for idx, user_id in enumerate(user_id_array):
-            if user_id in cold_user_id_array:
-                item_weights[idx] = item_weights_1[coldCounter]
-                coldCounter = coldCounter + 1
+
+        x = np.arange(0, self.n_users)
+        cold_users = x[self._cold_user_mask]
+        rankList = []
+        scoresList = []
+
+        for user in user_id_array:
+            if user in cold_users:
+                recommended_items, scores = self.topPop.recommend(user, cutoff, remove_seen_flag, items_to_compute, remove_top_pop_flag, remove_custom_items_flag, return_scores=True)
             else:
-                item_weights[idx] = item_weights_2[warmCounter]
-                warmCounter = warmCounter + 1
-        return item_weights
+                recommended_items, scores = self.CF.recommend(user, cutoff, remove_seen_flag, items_to_compute, remove_top_pop_flag, remove_custom_items_flag, return_scores=True)
+            rankList.append(recommended_items)
+            scoresList.append(scores)
+
+        # Return single list for one user, instead of list of lists
+        if single_user:
+            rankList = rankList[0]
+
+        if return_scores:
+            return rankList, np.array(scoresList).reshape((len(user_id_array), -1))
+        else:
+            return rankList
--- RecLib/ItemKNNScoresHybridRecommender.py
+++ RecLib/ItemKNNScoresHybridRecommender.py
@@ -9,19 +9,33 @@ class ItemKNNScoresHybridRecommender(BaseSimilarityMatrixRecommender):
 
     RECOMMENDER_NAME = "ItemKNNScoresHybridRecommender"
 
-    def __init__(self, URM_train, Recommender_1, Recommender_2):
+    def __init__(self, URM_train, Recommender_1, Recommender_2, *otherRecs):
         super(ItemKNNScoresHybridRecommender, self).__init__(URM_train)
-
         self.URM_train = Base.Recommender_utils.check_matrix(URM_train.copy(), 'csr')
-        self.Recommender_1 = Recommender_1
-        self.Recommender_2 = Recommender_2
+        self.RecList = []
+        self.RecList.append(Recommender_1)
+        self.RecList.append(Recommender_2)
+        for rec in otherRecs:
+            self.RecList.append(rec)
+        self.recLen = len(self.RecList)
 
-    def fit(self, alpha=0.5):
-        self.alpha = alpha
+    def fit(self, alpha=0.5, **otherParam):
+        self.params = otherParam
+        self.params['alpha'] = alpha
+
+        if self.recLen is not 2 and self.recLen is not len(self.params):
+            raise Exception('ERROR: parameters insufficient')
+        if self.recLen is 2 and len(self.params) is 0:
+            self.alphaOnly = True
+
 
     def _compute_item_score(self, user_id_array, items_to_compute):
-        item_weights_1 = self.Recommender_1._compute_item_score(user_id_array)
-        item_weights_2 = self.Recommender_2._compute_item_score(user_id_array)
-        item_weights = item_weights_1 * self.alpha + item_weights_2 * (1 - self.alpha)
+        item_weights = []
+        for rec in self.RecList:
+            item_weights.append(rec._compute_item_score(user_id_array))
+        if self.alphaOnly:
+            item_weights = item_weights[0] * self.params['alpha'] + item_weights[0]* (1 - self.params['alpha'])
+        else:
+            pass
 
         return item_weights
